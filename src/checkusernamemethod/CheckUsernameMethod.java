36b6375a2eb34003c9729370a1d19e92630e1c70
lab_services_student <lab_services_student@10.0.0.141>
2025-04-11 11:29:09 +0200

    First Commit

---
+++ src/checkusernamemethod/CheckUsernameMethod.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package checkusernamemethod;
+
+import java.util.Scanner;
+public class CheckUsernameMethod {
+
+    public static boolean checkusername(String username){
+        if(!username.contains("_")|| username.length()>5){
+            return false;
+        }else{
+            
+            return true;
+            
+        }
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner scan = new Scanner(System.in);
+        
+        // capture user details
+        System.out.println("Please enter your first name");
+        String firstname = scan.nextLine();
+        System.out.println("Please enter your last name");
+        String lastname= scan.nextLine();
+        
+        while (true){
+            System.out.println("Enter Username");
+            String username = scan.nextLine();
+            
+            if (checkusername(username)){
+                
+                System.out.println("Username successfully captured");
+                break;
+            }else{
+                System.out.println("Username is not correctly formatted");
+            }
+          
+                
+        }
+    }
+    
+}
